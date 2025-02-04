a1fb4cf765c62ac9441e62ab1a1d2a9211756076
MCarnegie <marcocarnegie4@gmail.com>
2025-02-03 22:12:33 -0500

    s1 2023 ccc completed

--- CCC Practice/2024 ccc/Main.java
+++ CCC Practice/2024 ccc/Main.java
@@ -1,11 +1,12 @@
 import java.io.*;
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class Main {
   public static void main(String [] args) throws Exception {
     Scanner in = new Scanner(System.in);
     int n = in.nextInt();
-    //hat circle S1 2024 CCC - full marks complete
+    //hat circle S1 2024 CCC - completed 15 points
     // int[] nums = new int[n];
     // for(int i = 0; i<n; i++){
     //     nums[i] = in.nextInt();
@@ -19,5 +20,61 @@ public class Main {
     //     }
     // }
     // System.out.println(counter);
+
+    //Triagalaine S1 2023 CCC - completed 15 points
+//     in.nextLine();
+//     String first = in.nextLine();
+
+//     String second = in.nextLine();
+//     in.close();
+
+//     String[] a1 = new String[n];
+//     int placer = 0;
+//     for(int i = 0; i<first.length(); i++){
+//         if(first.substring(i, i+1).equals(" ") == false){
+//             a1[placer] = first.substring(i, i+1);
+//             placer++;
+//         }
+//     }
+    
+//     String[] a2 = new String[n];
+//     placer = 0;
+//     for(int i = 0; i<second.length(); i++){
+//       if(second.substring(i, i+1).equals(" ") == false){
+//           a2[placer] = second.substring(i, i+1);
+//           placer++;
+//       }
+//   }
+//   //check every filled square: starts off with 3 for everything adjacent to it take off one
+//     int totalMeters = 0;
+//     for(int i = 0; i<n; i++){
+//         if(a1[i].equals("1")){
+//             int tapeAdd = 3;
+
+//             if(i!=n-1 && a1[i].equals(a1[i+1]))
+//                 tapeAdd--;
+//             if(i!=0 && a1[i].equals(a1[i-1]))
+//                 tapeAdd--;
+            
+//             totalMeters+=tapeAdd;
+//         }
+//         if(a2[i].equals("1")){
+//             int tapeAdd = 3;
+
+//             if(i!=n-1 && a2[i].equals(a2[i+1]))
+//                 tapeAdd--;
+//             if(i!=0 && a2[i].equals(a2[i-1]))
+//                 tapeAdd--;
+            
+//             totalMeters+=tapeAdd;
+//         }
+//         if(a2[i].equals("1") && a1[i].equals("1") && i%2 == 0)
+//             totalMeters-=2;
+//     }
+    
+//     System.out.println(totalMeters);
+
+
+    
   }
 }
